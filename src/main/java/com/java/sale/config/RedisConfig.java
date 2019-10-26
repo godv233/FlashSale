ec427ee9403be0b0514e837b8d45e3cb33b6f41d
godv233 <zengwei233@126.com>
2019-10-26 14:46:52 +0800

    配置redisTemplate和cacheManager

---
+++ src/main/java/com/java/sale/config/RedisConfig.java
@@ -0,0 +1,84 @@
+package com.java.sale.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+/**
+ * @author 曾伟
+ * @date 2019/10/26 14:07
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean //在没有指定缓存Key的情况下，key生成策略
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    // 缓存管理器 使用Lettuce，和jedis有很大不同
+    @Bean
+    public CacheManager cacheManager(LettuceConnectionFactory  lettuceConnectionFactory) {
+        //关键点，spring cache的注解使用的序列化都从这来，没有这个配置的话使用的jdk自己的序列化，实际上不影响使用，只是打印出来不适合人眼识别
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))//key序列化方式
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))//value序列化方式
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofHours(1));//缓存过期时间
+
+
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(lettuceConnectionFactory)
+                .cacheDefaults(config)
+                .transactionAware();
+
+        return builder.build();
+    }
+
+
+    /**
+     * RedisTemplate配置 在单独使用redisTemplate的时候 重新定义序列化方式
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer=new Jackson2JsonRedisSerializer(Object.class);
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        RedisSerializer<?> stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);// key序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);// value序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);// Hash key序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);// Hash value序列化
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    private RedisSerializer<String> keySerializer() {
+        return new StringRedisSerializer();
+    }
+
+}
--- src/main/java/com/java/sale/service/UserService.java
+++ src/main/java/com/java/sale/service/UserService.java
@@ -3,6 +3,7 @@ package com.java.sale.service;
 import com.java.sale.dao.mapper.UserDao;
 import com.java.sale.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserDao userDao;
+    @Cacheable(cacheNames = "user")
     public User getById(int id){
         return userDao.getUserById(id);
     }
